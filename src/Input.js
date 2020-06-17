f5aa71517743c50a94b3fa68af5a6da4e100ea37
sangi_imac <mrzero885@gmail.com>
2020-06-18 00:08:19 +0900

    인풋 태그 사용법

--- src/Input.js
+++ src/Input.js
@@ -2,25 +2,39 @@ import React,{useState} from 'react';
 
 function Inputer(){
 
-    const [text,settext] = useState("");
+    const [text,settext] = useState({
+        name:"",
+        nickname:""
+    });
+
+    const {name,nickname} = text;  //객체를 비구조화 할당할 때 사용함
 
     const onReset= ()=>{
-        settext("");
-
+        settext(
+            {
+                name:"",
+                nickname:""
+            }
+        );
     };
-
     const onPaint= (e)=>{
-        settext(e.target.value);
+        const {name, value} = e.target;
+
+        settext({
+            ...text,    // 자바스크립트 스프레드문법을 사용해서 객체를 복사해온다. 이를통해 불변성을 지킬수 있음.
+            [name]:value,
+        });
+        
     };
 
     return(
         <div style={{
             margin:"20px"
         }}>
-            <input placeholder="이름을 입력하세요" onChange={onPaint} value={text} />
-            <input placeholder="닉네임을 입력하세요" onChange={onPaint} value={text} />
+            <input name ="name" placeholder="이름을 입력하세요" onChange={onPaint} value={name} />
+            <input name ="nickname" placeholder="닉네임을 입력하세요" onChange={onPaint} value={nickname} />
             <button onClick={onReset}>초기화</button>
-            <p>값:{text}</p>
+            <p>이름:{name} 닉네임:{nickname}</p>
         </div>
     );
 }
